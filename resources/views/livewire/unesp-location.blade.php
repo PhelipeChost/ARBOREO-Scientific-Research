6e0ee36b5cc39a930ce0fbdce7c06f2f36c20992
Felipe Costa do Nascimento <thunderweb.log@gmail.com>
2024-10-14 20:08:10 -0300

    CORS e Imagem de visualização
    Correção do link da rota API para utilização do CORS (uso temporário)
    É possível agora expandir a imagem para uma melhor visualização

--- resources/views/livewire/unesp-location.blade.php
+++ resources/views/livewire/unesp-location.blade.php
@@ -23,10 +23,62 @@
             Helvetica,
             sans-serif;
         }
+        .modal {
+            display: none;
+            position: fixed;
+            z-index: 100;
+            padding-top: 50px;
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 100%;
+            overflow: auto;
+            background-color: rgba(0,0,0,0.8);
+        }
+
+        .modal-content {
+            margin: auto;
+            display: block;
+            width: 80%;
+            max-width: 700px;
+        }
+
+        .close {
+            position: absolute;
+            top: 15px;
+            right: 35px;
+            color: #fff;
+            font-size: 40px;
+            font-weight: bold;
+            cursor: pointer;
+        }
+
+        .close:hover,
+        .close:focus {
+            color: #bbb;
+            text-decoration: none;
+            cursor: pointer;
+        }
+
+        @keyframes zoom {
+            from {transform: scale(0)} 
+            to {transform: scale(1)}
+        }
+
+        .modal-content {
+            animation-name: zoom;
+            animation-duration: 0.6s;
+        }
     </style>
 </head>
 <body>
 <div id="map"></div>
+
+<!-- Modal para exibir a imagem em tamanho expandido -->
+<div id="imageModal" class="modal">
+    <span class="close">&times;</span>
+    <img class="modal-content" id="expandedImage">
+</div>
 
 <script>
     mapboxgl.accessToken = 'pk.eyJ1IjoibW9uaWtybyIsImEiOiJjbHYwMXM0MncxZnF1MmtvNXU4c252bDltIn0.nOOBwlBljykLvVNd-DT36w';
@@ -57,9 +109,9 @@
                     <strong>Família:</strong> ${item.planta.familia.nomefamilia} <br>
                     <strong>Local:</strong> ${item.planta.local.nomelocal} <br>
                     <strong>Descrição:</strong> ${item.descricao} <br>
-                    <img src="" id="img-${item.codimagem}" alt="Imagem da planta" style="max-width:100px; max-height:100px;">
+                    <img src="" id="img-${item.codimagem}" alt="Imagem da planta" class="thumbnail" style="max-width:100px; max-height:100px; cursor: pointer;">
                 `,
-                imgBlob: item.foto, // Armazenamos o blob aqui para carregar depois
+                imgBlob: item.foto,
                 codimagem: item.codimagem
             }
         }));
@@ -71,20 +123,15 @@
     }
 
     map.on('load', function() {
-        // Requisição para a API de imagens e plantas
-        fetch('http://inventarioarboreo.feis.unesp.br:8090/inventario/imagens')
+        fetch('https://cors-anywhere.herokuapp.com/http://inventarioarboreo.feis.unesp.br:8090/inventario/imagens')
             .then(response => response.json())
             .then(data => {
-                // Formata os dados no formato GeoJSON
                 const geojsonData = formatToGeoJSON(data);
-
-                // Adiciona a fonte de dados ao mapa
                 map.addSource('places', {
                     type: 'geojson',
                     data: geojsonData
                 });
 
-                // Adiciona uma camada para mostrar os pontos
                 map.addLayer({
                     id: 'places',
                     type: 'circle',
@@ -97,33 +144,33 @@
                     }
                 });
 
-                // Evento de clique no ponto
                 map.on('click', 'places', (e) => {
                     const coordinates = e.features[0].geometry.coordinates.slice();
                     const description = e.features[0].properties.description;
                     const imgBlob = e.features[0].properties.imgBlob;
                     const codimagem = e.features[0].properties.codimagem;
 
-                    // Gera o popup com as informações
                     new mapboxgl.Popup()
                         .setLngLat(coordinates)
                         .setHTML(description)
                         .addTo(map);
 
-                    // Converte o blob em URL da imagem e seta no elemento de imagem
                     fetch(`data:image/jpeg;base64,${imgBlob}`).then(response => response.blob())
                         .then(blob => {
                             const imageUrl = blobToImageURL(blob);
                             document.getElementById(`img-${codimagem}`).src = imageUrl;
+
+                            // Adiciona evento para expandir a imagem ao ser clicada
+                            document.getElementById(`img-${codimagem}`).addEventListener('click', function() {
+                                expandImage(imageUrl);
+                            });
                         });
                 });
 
-                // Quando o cursor estiver sobre o ponto
                 map.on('mouseenter', 'places', () => {
                     map.getCanvas().style.cursor = 'pointer';
                 });
 
-                // Quando o cursor sair do ponto
                 map.on('mouseleave', 'places', () => {
                     map.getCanvas().style.cursor = '';
                 });
@@ -133,14 +180,31 @@
             });
     });
 
-    // Adiciona controle de navegação e tela cheia
+    // Função para expandir a imagem
+    function expandImage(imageUrl) {
+        const modal = document.getElementById("imageModal");
+        const modalImg = document.getElementById("expandedImage");
+
+        modal.style.display = "block";
+        modalImg.src = imageUrl;
+
+        // Fecha o modal ao clicar no "x" ou fora da imagem
+        document.getElementsByClassName("close")[0].onclick = function() {
+            modal.style.display = "none";
+        };
+
+        modal.onclick = function() {
+            modal.style.display = "none";
+        };
+    }
+
     map.addControl(new mapboxgl.FullscreenControl({container: document.querySelector('body')}));
     map.addControl(new mapboxgl.NavigationControl());
 </script>
 
-
-</body>
 <script src="{{ url('https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js') }}" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <script src="{{ url('https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js') }}" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+</body>
+
 </html>
 
